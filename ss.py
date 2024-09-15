e17e59741e6b5bb19bdb2d8f165a7f53bea70b5f
arpit111mishra <arpit111mishra@gmail.com>
2024-09-15 13:24:20 +0530

    first commit

---
+++ ss.py
@@ -0,0 +1,27 @@
+import time
+import pyautogui
+import tkinter as tk
+def screenshot():
+    name= int(round(time.time()*1000))
+    name= r'/Volumes/Code/Screenshot Project/screenshot data{}.png'.format(name)
+    #time.sleep(5)
+    img = pyautogui.screenshot(name)
+    img.show()
+    
+root = tk.Tk()
+frame=tk.Frame(root)
+frame.pack()
+
+button= tk.Button(
+    frame,
+    text = "take screenshot",
+    command=screenshot)
+button.pack(side=tk.LEFT)
+
+close= tk.Button(
+    frame,
+    text = "Quit",
+    command=quit
+    )
+close.pack(side=tk.LEFT)
+root.mainloop()
